989763ec9a138ab337f9ee61c6d9df6927beb7e5
matheus <matheuslbernardi@gmail.com>
2024-09-15 22:04:18 -0300

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./App.css";
+import "./index.css";
+import { Button } from "./components/ui/button";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <p className="read-the-docs text-xl">Bernardic</p>
+      <div className="flex flex-col items-center justify-center gap-4 p-4">
+        <p className="font-custom text-xl">Bernardic</p>
+        <Button variant={"default"} onClick={() => setCount(count + 1)}>
+          Click me
+        </Button>
+        <p>{count}</p>
+      </div>
     </>
   );
 }
