0f1a7de8a6540e6fab56676b90bcc2f25586cb1b
matheus <matheuslbernardi@gmail.com>
2024-09-25 22:06:47 -0300

    augment word type

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,8 @@
 export type Word = {
   id: number;
+  type: "verb" | "noun" | "adjective" | "adverb";
   word: string;
-  definition: string;
+  definition: string[];
   examples: string[];
   synonyms: string[];
   // audio: string;
@@ -10,40 +11,49 @@ export type Word = {
 export const words: Word[] = [
   {
     id: 1,
+    type: "adjective",
     word: "op",
-    definition:
+    definition: [
       "Quando uma coisa é muito boa, excelente, maravilhosa, incrível, espetacular, etc.",
+    ],
     examples: ["Esse filme é op demais!"],
     synonyms: ["forte", "bom", "incrível"],
   },
   {
     id: 2,
+    type: "adjective",
     word: "forte",
-    definition:
+    definition: [
       "Quando uma coisa é muito boa, excelente, maravilhosa, incrível, espetacular, etc.",
+    ],
     examples: ["Esse filme é forte demais!"],
     synonyms: ["op", "bom", "incrível"],
   },
   {
     id: 3,
+    type: "verb",
     word: "chama",
-    definition:
+    definition: [
       "Quando se está muito empolgado com algo; eleva a moral de alguém.",
+    ],
     examples: ["Nossa, mandou bem! Chama!"],
     synonyms: ["vamo", "dale"],
   },
   {
     id: 4,
+    type: "verb",
     word: "vamo",
-    definition:
+    definition: [
       "Quando se está muito empolgado com algo; eleva a moral de alguém.",
+    ],
     examples: ["Nossa, mandou bem! Vamo!"],
     synonyms: ["chama", "dale"],
   },
   {
     id: 5,
+    type: "adjective",
     word: "troll",
-    definition: "Pessoa com atitudes desagradáveis e/ou provocativas.",
+    definition: ["Pessoa com atitudes desagradáveis e/ou provocativas."],
     examples: ["Esse cara é um troll."],
     synonyms: ["meme", "podre", "crime"],
   },
